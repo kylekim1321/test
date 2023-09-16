4539f80a1bca0ba686688dee6ee0ebe9b2c964c0
Namgung soo <namgungsoo>
2023-09-16 09:11:41 +0900

    week04 chap02 v0.8

---
+++ week03.py
@@ -0,0 +1,17 @@
+x = [1, 2, 3]
+y = x
+print(x,y)
+x[0] = -11
+print(x,y)
+#dictionary : mutable
+human = {'eye' : [1.0, 0.9], 'blood_type' : 'ab'}
+print(human['eye'])
+human['eye'][0] = 0.7
+print
+
+
+#a = 1
+#b = a
+#print(a,b)
+#a = 2
+#print(a,b)
