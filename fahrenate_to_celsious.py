a4b5c52e12f3acfa490a25489046710f39eaeed9
Namgung soo <namgungsoo>
2023-09-16 10:29:10 +0900

    week04 chap02

---
+++ fahrenate_to_celsious.py
@@ -0,0 +1,4 @@
+fahrenheit = input("Input temperature in fahrenheit : ")
+# (100°F − 32) × 5/9 = 37.778°C
+celsius = (int(fahrenheit) - 32) * 5/9
+print(f"fahrenheit {fahrenheit}F equal to Celsius {celsius}C. ")
--- week04.py
+++ week04.py
@@ -1,14 +1,13 @@
-a = 11//2
-print(a)
-a = 11 % 2
-print(a)
-a = divmod(11, 2)
-print(a)
-print(a[0])
-print(a[1])
-b = 2 * 2 * 2 * 2 * 2
-print(b)
-b = 2 ** 5
-print(b)
-b = pow(2, 5)
-print(b)
+a = 0xAC71
+b = 0o1010110001110001
+c = 0o126161
+d = 44145
+print(a, b, c, d)
+print(hex(d), bin(d), oct(b), d)
+print(chr(66))
+print(ord('8'))
+print(chr(98))
+print(ord('8'))
+print(ord('8'))
+print(chr(0xAE40))
+
