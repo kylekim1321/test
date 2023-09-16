aebaa79bcf2241a6c19313478ba59b4adb791c6d
Namgung soo <namgungsoo>
2023-09-16 11:36:14 +0900

    week04. chap03.v1.0

--- week04. chap03.py
+++ week04. chap03.py
@@ -1,7 +1,6 @@
-print(bool(None))
-print(bool(''))
-print(bool([]))
-print(bool(()))
-print(bool({}))
-print(bool(set()))
-print(bool(0))
+vowels = {'a', 'e', 'i', 'o', 'u'}
+letter = input("input alphabet letter : ")
+if letter in vowels:
+    print(f"{letter} is a vowel~")
+else:
+    print(f"{letter} is not a vowel!")
