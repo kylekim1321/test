267a87f46d3630ef8b3eee8deb59783b74210447
Namgung soo <namgungsoo>
2023-10-28 10:34:34 +0900

    week07.chap01

--- week07. chap01.py
+++ week07. chap01.py
@@ -1,15 +1,9 @@
 numbers = list()
 for i in range(0, 11):
-    #print(i, end=' ')
     numbers.append(i)
 print(numbers)
-odd_numbers = numbers[1::2]
-even_numbers = numbers[::2]
-print(numbers[-2])
-print(numbers[-11], numbers[0])
-reverse_odd_numbers = numbers[-2::-2]
-reverse_numbers = numbers.reverse() # reverse_numbers = numbers[::-1]
-print(odd_numbers)
-print(even_numbers)
-print(reverse_odd_numbers)
+reverse_numbers = numbers[::-1]
 print(reverse_numbers)
+print(numbers)
+numbers.reverse() #자체 값 변경
+print(numbers)
