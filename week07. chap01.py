03bc7ba2657d0ed2e27ab0128bfcd48994d5bf71
Namgung soo <namgungsoo>
2023-10-28 10:43:43 +0900

    week07.chap01

--- week07. chap01.py
+++ week07. chap01.py
@@ -7,3 +7,10 @@ print(reverse_numbers)
 print(numbers)
 numbers.reverse() #자체 값 변경
 print(numbers)
+numbers.append(-11)
+print(numbers)
+numbers.insert(__index=5, __object=99)
+print(numbers)
+numbers.insert(-2, __object=55) #list multiplication
+numbers = numbers * 2
+print(numbers)
