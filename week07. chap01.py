d96cacd394413939b7e184834ca8c9dd6ece7321
Namgung soo <namgungsoo>
2023-11-04 09:14:29 +0900

    week08.chap01

--- week07. chap01.py
+++ week07. chap01.py
@@ -2,6 +2,8 @@ a = [1, 9, 11]
 b = a
 print(a, b)
 a[1] = 55
-print(a, b) # b가 참조하는 객체와 a가 참조하는 객체가 같다.
+print(a, b) # b가 참조 하는 객체와 a가 참조 하는 객체가 같다.
 b[-1] = "Python"
-print(a, b) # b가 참조하는 객체와 a가 참조하는 객체가 같다.
+print(a, b)
+for i in range(2, -1, -1):
+    print(i)
