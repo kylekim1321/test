c069c25734f82404f56d058c5c6db480935087a8
Namgung soo <namgungsoo>
2023-11-04 12:16:27 +0900

    week08.chap02

--- week08.chap01.py
+++ week08.chap01.py
@@ -1,11 +1,10 @@
-a = [-11, 72, 9]
+a = [-11, [3, 0], 9]
 b = a.copy()
 c = list(a)
 d = a[:]
 e = a
 
 print(a, b, c, d, e)
-a[1] = 2.71
-print(a, b, c, d, e)
-e[0] = "ishs"
+a[1][0] = 7
+# a[1] = 99
 print(a, b, c, d, e)
