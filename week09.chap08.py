d4cc98d40043b2c8a28509c8e2ad4cdcab884a77
Namgung soo <namgungsoo>
2023-11-18 10:39:14 +0900

    week09.chap08 v2

--- week09.chap08.py
+++ week09.chap08.py
@@ -1,21 +1,13 @@
-subjects = {'python': 5, 'java': 9}
+subjects1 = {'python': 5, 'java' : 9}
+subjects2 = {'data structure': 5, 'java' : 9, 'go': [7, 8]}
+subjects3 = subjects2
+subjects4 = dict(subjects2)
+subjects = {**subjects1, **subjects2}
 print(subjects)
-print(subjects['java']) # access
-subjects['go'] = 7 # add
+subjects2['java'] = 11
+subjects2['go'][0] = 12
 print(subjects)
-subjects['java'] = 8 #update
-print(subjects)
-
-for subjects in subjects:
-    print(subjects)
-
-for subjects in subjects.items():
-    print(subjects)
-
-for subject, students in subjects.items():
-    print(subjects, students)
-    print(f" {subject} 과목을 수강하는 학생의 인원은  {students}명 입니다")
-
-del subjects['java'] # delete
-print(subjects)
-
+print(subjects1)
+print(subjects2)
+print(subjects3)
+print(subjects4)
