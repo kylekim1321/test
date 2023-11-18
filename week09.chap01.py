187509a7922edf6d33c4916258062d7511440de1
Namgung soo <namgungsoo>
2023-11-18 09:38:55 +0900

    weeko9.chap01.py v2

--- week09.chap01.py
+++ week09.chap01.py
@@ -2,6 +2,9 @@
 import random
 
 dices = []
-for i in range(5)
+for i in range(5):
     dices.append(random.randint(1,6))
+print(dices)
+
+dices =[random.randint(1,6) for i in range(5)]
 print(dices)
